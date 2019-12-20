d450bb20261a37f15fcbd8f34be2346f808d30d3
Wu <haifwu@packer-5de945cf-2566-f44e-1516-892035f969aa.c.pp-dev-cloudsystems.internal>
2019-12-19 23:36:26 -0800

    add data folder

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-sudo docker run -d -v $(pwd)/etc:/opt/aerospike/etc --name aerospike -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike asd --foreground --config-file /opt/aerospike/etc/aerospike.conf
-
+sudo docker run -tid -v /home/haifwu/aerospike/etc:/opt/aerospike/etc -v /home/haifwu/aerospike/data:/opt/aerospike/data --name aerospike -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike /usr/bin/asd --foreground --config-file /opt/aerospike/etc/aerospike.conf
