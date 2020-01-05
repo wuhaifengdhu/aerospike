71c4eda29ee2020279b6e7f98908ced170c0e4ff
Devin <haifwu@paypal.com>
2020-01-05 17:11:59 +0800

    update script

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo docker run -tid -v /home/haifwu/aerospike/etc:/opt/aerospike/etc -v /home/haifwu/aerospike/data:/opt/aerospike/data --name aerospike -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike /usr/bin/asd --foreground --config-file /opt/aerospike/etc/aerospike.conf
+sudo docker run -tid -v $(pwd)/etc:/opt/aerospike/etc -v $(pwd)/data:/opt/aerospike/data --name aerospike -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike /usr/bin/asd --foreground --config-file /opt/aerospike/etc/aerospike.conf
